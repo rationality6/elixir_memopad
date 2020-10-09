2af5d9bef154673518252cf50fda99a95b790a30
Hyun Ahn <codepawn@icloud.com>
2020-10-09 16:24:54 +0900

    validation 만들면서

---
+++ rand002332.exs
@@ -0,0 +1,30 @@
+Enum.random(0..10)
+|> Kernel.*(10)
+|> IO.inspect()
+
+Enum.random(0..10)
+|> Kernel.*(10)
+|> IO.inspect()
+
+rem(10,33)
+|>IO.inspect()
+
+0..6
+|> Enum.chunk_every(4,1)
+|> IO.inspect()
+
+0..6
+|> Enum.chunk_every(3,1)
+|> IO.inspect()
+
+0..8
+|> Enum.chunk_every(4,1)
+|> IO.inspect()
+|> Enum.reverse() |> tl() |> Enum.reverse()
+|> IO.inspect()
+
+min(103,100)
+|>IO.inspect()
+
+min(103,100)
+|>IO.inspect()
